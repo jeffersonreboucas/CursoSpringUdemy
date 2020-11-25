7d9d27da58e986b2be0d9bf07d075534e0038352
jeffersonreboucas@hotmail.com <jeffersonreboucas@hotmail.com>
2020-11-25 18:24:14 +0000

    Criando operação de instaciação

--- src/main/java/com/jeffersonreboucas/cursoSpring/CursoSpringApplication.java
+++ src/main/java/com/jeffersonreboucas/cursoSpring/CursoSpringApplication.java
@@ -1,13 +1,34 @@
 package com.jeffersonreboucas.cursoSpring;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoSpringApplication {
+import com.jeffersonreboucas.cursoSpring.domain.Categoria;
+import com.jeffersonreboucas.cursoSpring.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursoSpringApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSpringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
