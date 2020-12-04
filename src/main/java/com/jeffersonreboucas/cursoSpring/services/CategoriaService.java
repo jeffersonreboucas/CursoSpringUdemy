f40941e5fa7375967b4c46e3b363108c73151d90
jeffersonreboucas@hotmail.com <jeffersonreboucas@hotmail.com>
2020-12-04 17:45:52 +0000

    Ajustes no endpoint /Categoria/{id}

--- src/main/java/com/jeffersonreboucas/cursoSpring/services/CategoriaService.java
+++ src/main/java/com/jeffersonreboucas/cursoSpring/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.jeffersonreboucas.cursoSpring.domain.Categoria;
 import com.jeffersonreboucas.cursoSpring.repositories.CategoriaRepository;
+import com.jeffersonreboucas.cursoSpring.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,9 @@ public class CategoriaService {
 	
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
-		}
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+		
+	}
 
 }
